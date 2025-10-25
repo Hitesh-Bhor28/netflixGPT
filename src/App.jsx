02c0a10f07b8546379d25fc9026a8bb697d7e513
Hitesh Bhor <hiteshbhor28@gmail.com>
2025-10-25 13:25:58 +0530

    Basic folder structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='flex justify-center items-center h-screen '>
+        <h1 className='text-9xl text-amber-800 text-shadow-cyan-400'>hii</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
